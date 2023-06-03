eee5453c9681ecb99267136bcad683cbf8b7a54b
Ришат Вергасов <Zvezda-Rishat1991@yandex.ru>
2023-06-03 22:15:53 +0300

    pdf

--- backend/foodgram/api/utils.py
+++ backend/foodgram/api/utils.py
@@ -6,22 +6,6 @@ from io import BytesIO
 from api.serializers import RecipeIngredients
 
 
-""" def download_shopping_cart(self, request):
-    ingredients = RecipeIngredients.objects.filter(
-        recipe__shopping__user=request.user).values(
-            'ingredient__name', 'ingredient__measurement_unit').annotate(
-                amount=Sum('amount'))
-    text = ''
-    for ingredient in ingredients:
-        text += (f'•  {ingredient["ingredient__name"]}'
-                 f'({ingredient["ingredient__measurement_unit"]})'
-                 f'— {ingredient["amount"]}\n')
-    headers = {
-        'Content-Disposition': 'attchment; filename=shoping_cart.txt'}
-    return HttpResponse(
-        text, content_type='text/plain; charset=UTF-8', headers=headers) """
-
-
 def download_shopping_cart(self, request):
     ingredients = RecipeIngredients.objects.filter(
         recipe__cart__user=request.user).values_list(
@@ -51,3 +35,19 @@ def download_shopping_cart(self, request):
     response['Content-Disposition'] = ('attachment; '
                                        'filename="shopping_list.pdf"')
     return response
+
+
+    """ def download_shopping_cart(self, request):
+    ingredients = RecipeIngredients.objects.filter(
+        recipe__shopping__user=request.user).values(
+            'ingredient__name', 'ingredient__measurement_unit').annotate(
+                amount=Sum('amount'))
+    text = ''
+    for ingredient in ingredients:
+        text += (f'•  {ingredient["ingredient__name"]}'
+                 f'({ingredient["ingredient__measurement_unit"]})'
+                 f'— {ingredient["amount"]}\n')
+    headers = {
+        'Content-Disposition': 'attchment; filename=shoping_cart.txt'}
+    return HttpResponse(
+        text, content_type='text/plain; charset=UTF-8', headers=headers) """
