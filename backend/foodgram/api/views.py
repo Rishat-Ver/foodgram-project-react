8b97e816bce172aed37fb2928d623d365f54e85f
Ришат Вергасов <Zvezda-Rishat1991@yandex.ru>
2023-06-04 02:23:27 +0300

    pdf

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -5,9 +5,6 @@ from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import SAFE_METHODS, IsAuthenticated
 from rest_framework.response import Response
-from django.http import FileResponse
-from reportlab.pdfgen import canvas
-from io import BytesIO
 
 from api.filters import NameSearchFilter, RecipeFilter
 from api.pagination import CustumPagination
@@ -171,31 +168,4 @@ class RecipeViewSet(viewsets.ModelViewSet):
             methods=['get'],
             permission_classes=[IsAuthenticated])
     def download_shopping_cart(self, request):
-        ingredients = RecipeIngredients.objects.filter(
-        recipe__cart__user=request.user).values_list(
-        'ingredient__name', 'ingredient__measurement_unit',
-        'amount')
-        cart_list = {}
-        for name, amount, unit in ingredients:
-            if name not in cart_list:
-                cart_list[name] = {"amount": amount, "unit": unit}
-            else:
-                cart_list[name]["amount"] += amount
-        height = 700
-        buffer = BytesIO()
-
-        page = canvas.Canvas(buffer)
-
-        page.drawString(100, 750, "Список покупок")
-        for i, (name, data) in enumerate(cart_list.items(), start=1):
-            page.drawString(
-                80, height, f"{i}. {name} – {data['amount']} {data['unit']}"
-            )
-            height -= 25
-        page.showPage()
-        page.save()
-        buffer.seek(0)
-        response = FileResponse(buffer, content_type='application/pdf')
-        response['Content-Disposition'] = ('attachment; '
-                                        'filename="shopping_list.pdf"')
-        return response
+        return download_cart(self, request)
