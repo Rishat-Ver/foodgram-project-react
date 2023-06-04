bab1f536a6718b5340fbe62e3b33704077c532a9
Ришат Вергасов <Zvezda-Rishat1991@yandex.ru>
2023-06-04 14:07:17 +0300

    pdf2

--- backend/foodgram/api/utils.py
+++ backend/foodgram/api/utils.py
@@ -10,9 +10,9 @@ from api.serializers import RecipeIngredients
 
 def download_cart(request):
     ingredients = RecipeIngredients.objects.filter(
-        recipe__shopping__user=request.user).values(
-            'ingredient__name', 'ingredient__measurement_unit').annotate(
-            amount=Sum('amount'))
+        recipe__shopping__user=request.user).values_list(
+        'ingredient__name', 'ingredient__measurement_unit',
+        'amount')
     cart_list = {}
     for item in ingredients:
         name = item[0]
